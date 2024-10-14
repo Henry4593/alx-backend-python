b73213f9ab17c2dd343faef1c49dad709a43c959
Henry4593 <jonyango4@gmail.com>
2024-10-14 13:24:50 +0300

    add map feature

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -21,10 +21,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     Returns:
         List[float]: A list of delays in ascending order.
     """
-    # Create a list of tasks
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    # Wait for all tasks to complete
-    delays = await asyncio.gather(*tasks)
-    # Sort delays without using sort()
-    sorted_delays = sorted(delays)
-    return sorted_delays
+    wait_time = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_time)
